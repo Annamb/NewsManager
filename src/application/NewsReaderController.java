18d51d182f3f34394a046349491d55cf239f5b15
Annamb <annamb97@gmail.com>
2021-10-28 16:31:30 +0200

    list on main page

--- src/application/NewsReaderController.java
+++ src/application/NewsReaderController.java
@@ -8,7 +8,6 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.function.Predicate;
 
-
 import application.news.Article;
 import application.news.Categories;
 import application.news.User;
@@ -17,6 +16,8 @@ import application.utils.exceptions.ErrorMalFormedArticle;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -25,7 +26,10 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -42,28 +46,61 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import serverConection.ConnectionManager;
 
 /**
- * @author √ÅngelLucas
+ * @author √ngelLucas
  *
  */
 public class NewsReaderController {
 
+	@FXML
+    private ListView<Article> articleList;
+	
+	@FXML
+	private Label articleName;
+	
+//	@FXML
+//	private ComboBox<String> categoryCombo;
+	
 	private NewsReaderModel newsReaderModel = new NewsReaderModel();
 	private User usr;
+	
+	Article chosenArticle;
 
 	//TODO add attributes and methods as needed
+
 
 	public NewsReaderController() {
 		//TODO
 		//Uncomment next sentence to use data from server instead dummy data
 		//newsReaderModel.setDummyData(false);
 		//Get text Label
-		
 	}
 
 		
 
 	private void getData() {
 		//TODO retrieve data and update UI
+		 newsReaderModel.retrieveData();
+		 articleList.getItems().addAll(newsReaderModel.getArticles());
+
+//		 articleList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Article>() {
+//				 @Override
+//					/**
+//					 * When the selected element is changed this event handler is called
+//					 */
+//					public void changed(ObservableValue<? extends Article> observable, Article oldValue, Article newValue) {
+//						if (newValue != null){
+//							chosenArticle = newValue;
+//							articleName.setText("breytt");
+////							articleName.setText(newValue.getTitle());
+//							//btnNext.setDisable(false);
+//							//contactSelected.setText(newValue.toString());
+//						}
+//						else { //Nothing selected
+//							//contactSelected.setText("");
+//							//btnNext.setDisable(true);
+//						}
+//					}
+//		 });
 		//The method newsReaderModel.retrieveData() can be used to retrieve data  
 	}
 
